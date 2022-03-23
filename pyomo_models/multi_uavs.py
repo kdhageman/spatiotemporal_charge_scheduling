ce401dd181ba919ae539b6280e07ec8873413c85
Kaspar Hageman <kasparhageman@gmail.com>
2022-03-23 11:20:07 +0100

    Rename variables

--- pyomo_models/multi_uavs.py
+++ pyomo_models/multi_uavs.py
@@ -1,6 +1,6 @@
 import pyomo.environ as pyo
 
-from models.base import BaseModel
+from pyomo_models.base import BaseModel
 
 
 class MultiUavModel(BaseModel):
@@ -15,16 +15,16 @@ class MultiUavModel(BaseModel):
         # state variables
         self.Z_s = pyo.Var(self.d, self.w_s)
         self.Z_e = pyo.Var(self.d, self.w_s)
-        self.C = pyo.Var(self.d, self.d, self.w_s, self.w_s)
-        self.C_prime = pyo.Var(self.d, self.d, self.w_s, self.w_s)
+        self.Y = pyo.Var(self.d, self.d, self.w_s, self.w_s)
+        self.Y_prime = pyo.Var(self.d, self.d, self.w_s, self.w_s)
 
         # CONSTRAINTS
         self.Z_s_calc = pyo.Constraint(
             self.d,
             self.w_s,
             rule=lambda m, d, w_s: m.Z_s[d, w_s] == sum(
-                m.C[d, w_p] + m.F[d, w_p] + m.t(d, w_p) for w_p in range(w_s)) + sum(
-                m.P[d, n, w_s] * self.T_W[d, n, w_s] for n in m.n) + m.F[d, w_s]
+                m.C[d, w_p] + m.W[d, w_p] + m.t(d, w_p) for w_p in range(w_s)) + sum(
+                m.P[d, n, w_s] * self.T_W[d, n, w_s] for n in m.n) / self.v[d] + m.W[d, w_s]
         )
 
         self.Z_e_calc = pyo.Constraint(
@@ -33,30 +33,30 @@ class MultiUavModel(BaseModel):
             rule=lambda m, d, w_s: m.Z_e[d, w_s] == m.Z_s[d, w_s] + m.C[d, w_s]
         )
 
-        self.C_calc = pyo.Constraint(
+        self.Y_calc = pyo.Constraint(
             self.d,
             self.d,
             self.w_s,
             self.w_s,
-            rule=lambda m, d, d_prime, w_s, w_s_prime: m.C[d, d_prime, w_s, w_s_prime] == m.alpha[
+            rule=lambda m, d, d_prime, w_s, w_s_prime: m.Y[d, d_prime, w_s, w_s_prime] == m.alpha[
                 d, d_prime, w_s, w_s_prime] * (m.Z_s[d_prime, w_s_prime] - m.Z_s[d, w_s]) + (
                                                                1 - m.alpha[d, d_prime, w_s, w_s_prime]) * (
                                                                m.Z_e[d, w_s] - m.Z_s[d_prime, w_s_prime])
         )
 
-        self.C_prime_calc = pyo.Constraint(
+        self.Y_prime_calc = pyo.Constraint(
             self.d,
             self.d,
             self.w_s,
             self.w_s,
-            rule=lambda m, d, d_prime, w_s, w_s_prime: m.C_prime[d, d_prime, w_s, w_s_prime] == m.C[
+            rule=lambda m, d, d_prime, w_s, w_s_prime: m.Y_prime[d, d_prime, w_s, w_s_prime] == m.Y[
                 d, d_prime, w_s, w_s_prime] * sum(m.P[d, s, w_s] * m.P[d_prime, s, w_s_prime] for s in m.s)
         )
 
-        def C_prime_lim_rule(m, d, d_prime, w_s, w_s_prime):
+        def Y_prime_lim_rule(m, d, d_prime, w_s, w_s_prime):
             if d == d_prime:
                 return pyo.Constraint.Skip
-            return m.C_prime[d, d_prime, w_s, w_s_prime] <= 0
+            return m.Y_prime[d, d_prime, w_s, w_s_prime] <= 0
 
-        self.C_prime_lim = pyo.Constraint(self.d, self.d, self.w_s, self.w_s, rule=C_prime_lim_rule)
+        self.Y_prime_lim = pyo.Constraint(self.d, self.d, self.w_s, self.w_s, rule=Y_prime_lim_rule)
 
