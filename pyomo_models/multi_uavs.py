178db4f27ba8c2390a4ffc116ada9e6bf144e5a3
Kaspar Hageman <kasparhageman@gmail.com>
2022-05-18 11:52:56 +0200

    Remove additional linear constraints in multi-uav model

--- pyomo_models/multi_uavs.py
+++ pyomo_models/multi_uavs.py
@@ -13,93 +13,64 @@ class MultiUavModel(BaseModel):
 
         # VARIABLES
         self.o = pyo.Var(self.d, self.d, self.w_s, self.w_s, self.s, domain=pyo.Binary)
-        self.O = pyo.Var(self.d, self.d, self.w_s, self.w_s, domain=pyo.Binary)
         self.y = pyo.Var(self.d, self.d, self.w_s, self.w_s, domain=pyo.Binary)
-        self.T_s = pyo.Var(self.d, self.w_s)
-        self.T_e = pyo.Var(self.d, self.w_s)
 
         # CONSTRAINTS
-        self.Z_s_calc = pyo.Constraint(
-            self.d,
-            self.w_s,
-            rule=lambda m, d, w_s: m.T_s[d, w_s] == sum(
-                m.C[d, w_p] + m.W[d, w_p] + m.t(d, w_p) for w_p in range(w_s)) + sum(
-                m.P[d, n, w_s] * self.D_N[d, n, w_s] for n in m.n) / self.v[d] + m.W[d, w_s]
-        )
 
-        self.Z_e_calc = pyo.Constraint(
-            self.d,
-            self.w_s,
-            rule=lambda m, d, w_s: m.T_e[d, w_s] == m.T_s[d, w_s] + m.C[d, w_s]
-        )
-
-        def a_1_rule(m, d, d_prime, w_s, w_s_prime, s):
+        def o_1_rule(m, d, d_prime, w_s, w_s_prime, s):
             if d == d_prime:
                 return pyo.Constraint.Skip
             return m.o[d, d_prime, w_s, w_s_prime, s] <= m.P[d, s, w_s]
 
-        self.a_1 = pyo.Constraint(
+        self.o_1 = pyo.Constraint(
             self.d,
             self.d,
             self.w_s,
             self.w_s,
             self.s,
-            rule=a_1_rule
+            rule=o_1_rule
         )
 
-        def a_2_rule(m, d, d_prime, w_s, w_s_prime, s):
+        def o_2_rule(m, d, d_prime, w_s, w_s_prime, s):
             if d == d_prime:
                 return pyo.Constraint.Skip
             return m.o[d, d_prime, w_s, w_s_prime, s] <= m.P[d_prime, s, w_s_prime]
 
-        self.a_2 = pyo.Constraint(
+        self.o_2 = pyo.Constraint(
             self.d,
             self.d,
             self.w_s,
             self.w_s,
             self.s,
-            rule=a_2_rule
+            rule=o_2_rule
         )
 
-        def a_3_rule(m, d, d_prime, w_s, w_s_prime, s):
+        def o_3_rule(m, d, d_prime, w_s, w_s_prime, s):
             if d == d_prime:
                 return pyo.Constraint.Skip
             return m.o[d, d_prime, w_s, w_s_prime, s] >= m.P[d, s, w_s] + m.P[d_prime, s, w_s_prime] - 1
 
-        self.a_3 = pyo.Constraint(
+        self.o_3 = pyo.Constraint(
             self.d,
             self.d,
             self.w_s,
             self.w_s,
             self.s,
-            rule=a_3_rule
-        )
-
-        def A_calc_rule(m, d, d_prime, w_s, w_s_prime):
-            if d == d_prime:
-                return pyo.Constraint.Skip
-            return m.O[d, d_prime, w_s, w_s_prime] == sum(m.o[d, d_prime, w_s, w_s_prime, s] for s in m.s)
-
-        self.A_calc = pyo.Constraint(
-            self.d,
-            self.d,
-            self.w_s,
-            self.w_s,
-            rule=A_calc_rule
+            rule=o_3_rule
         )
 
         # window constraints
         def window_i_rule(m, d, d_prime, w_s, w_s_prime):
             if d == d_prime:
                 return pyo.Constraint.Skip
-            return m.T_e[d, w_s] <= m.T_s[d_prime, w_s_prime] - self.epsilon + self.M_1 * (
-                        1 + m.y[d, d_prime, w_s, w_s_prime] - m.O[d, d_prime, w_s, w_s_prime])
+            return m.T_e(d, w_s) <= m.T_s(d_prime, w_s_prime) - self.epsilon + self.M_1 * (
+                    1 + m.y[d, d_prime, w_s, w_s_prime] - m.O(d, d_prime, w_s, w_s_prime))
 
         def window_ii_rule(m, d, d_prime, w_s, w_s_prime):
             if d == d_prime:
                 return pyo.Constraint.Skip
-            return m.T_s[d_prime, w_s_prime] <= m.T_s[d, w_s] - self.epsilon + self.M_2 * (
-                        2 - m.y[d, d_prime, w_s, w_s_prime] - m.O[d, d_prime, w_s, w_s_prime])
+            return m.T_s(d_prime, w_s_prime) <= m.T_s(d, w_s) - self.epsilon + self.M_2 * (
+                    2 - m.y[d, d_prime, w_s, w_s_prime] - m.O(d, d_prime, w_s, w_s_prime))
 
         self.window_i = pyo.Constraint(self.d, self.d, self.w_s, self.w_s, rule=window_i_rule)
         self.window_ii = pyo.Constraint(self.d, self.d, self.w_s, self.w_s, rule=window_ii_rule)
@@ -107,3 +78,14 @@ class MultiUavModel(BaseModel):
     @property
     def W_max(self):
         return sum([self.C_max[d] + self.epsilon for d in self.d])
+
+    def T_s(self, d, w_s):
+        return sum(
+            self.C[d, w_p] + self.W[d, w_p] + self.t(d, w_p) for w_p in range(w_s)) + sum(
+            self.P[d, n, w_s] * self.D_N[d, n, w_s] for n in self.n) / self.v[d] + self.W[d, w_s]
+
+    def T_e(self, d, w_s):
+        return self.T_s(d, w_s) + self.C[d, w_s]
+
+    def O(self, d, d_prime, w_s, w_s_prime):
+        return sum(self.o[d, d_prime, w_s, w_s_prime, s] for s in self.s)
