fd8233d57a4313e5ec23d1fcf542fa4012287943
Kaspar Hageman <kasparhageman@gmail.com>
2022-12-15 08:29:40 +0100

    Revise utilization computation

--- notebooks_experiments/util.py
+++ notebooks_experiments/util.py
@@ -51,46 +51,13 @@ def load_results_from_dir(rootdir):
     df['trial'] = df.trial.astype(int)
     df['rescheduled'] = df.pi != np.inf
 
-    # determine experiment type
-    def experiment_type(x):
-        if x.scheduler == 'naivescheduler':
-            return 'naive'
-        if x.pi != np.inf:
-            if x.W_hat == 10:
-                return 'fixed_w_hat_10'
-            if x.W_hat == 15:
-                return 'fixed_w_hat_15'
-            if x.W_hat == 40:
-                return 'fixed_w_hat_40'
-            else:
-                raise Exception("unknown experiment..")
-        if x.sigma == 3:
-            return 'sigma3'
-        if x.sigma == 2:
-            return 'sigma2'
-        return 'optimal'
-
-    df['experiment_type'] = df.apply(experiment_type, axis=1)
-
     return df
 
-
 def get_charging_station_utilization(parsed):
-    """
-    Return the utilization for each of the charging station in the parsed results
-    The utilization is expressed as:
-      U = (total time charged at station + total time waited at station) / execution_time
-      
-    @return: the utilization, the time charged and the time waited for each station
-    """
-    execution_time = parsed['execution_time']
+    mission_time_cumulative = sum([l[-1]['t_end'] for l in parsed['event']])
 
-    time_charged_all = {
-        i: 0 for i in range(parsed['scenario']['nr_charging_stations'])
-    }
-    time_waited_all = {
-        i: 0 for i in range(parsed['scenario']['nr_charging_stations'])
-    }
+    time_charged_all = {}
+    time_waited_all = {}
 
     for event_list in parsed['event']:
         for ev in event_list:
@@ -98,16 +65,15 @@ def get_charging_station_utilization(parsed):
                 station_idx = ev['node']['id']
                 t_start = ev['t_start']
                 t_end = ev['t_end']
-                time_charged_all[station_idx] += t_end - t_start
+                time_charged_all[station_idx] = time_charged_all.get(station_idx, 0) + t_end - t_start
             elif ev['type'] == 'waited':
                 station_idx = ev['node']['id']
                 t_start = ev['t_start']
                 t_end = ev['t_end']
-                time_waited_all[station_idx] += t_end - t_start
-    time_charged_all = list([v / execution_time for v in time_charged_all.values()])
-    time_waited_all = list([v / execution_time for v in time_waited_all.values()])
+                time_waited_all[station_idx] = time_waited_all.get(station_idx, 0) + t_end - t_start
 
-    utilization = []
-    for time_charged, time_waited in zip(time_charged_all, time_waited_all):
-        utilization.append(time_charged + time_waited)
-    return utilization, time_charged_all, time_waited_all
+    frac_charged = sum(time_charged_all.values()) / mission_time_cumulative
+    frac_waited = sum(time_waited_all.values()) / mission_time_cumulative
+    utilization = frac_charged + frac_waited
+    
+    return utilization, frac_charged, frac_waited
