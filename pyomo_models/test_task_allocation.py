903a96b9468fd156d6c6d81cd5aadccfda593cc8
Kaspar Hageman <kasparhageman@gmail.com>
2023-02-01 12:00:48 +0100

    Remove more redundant configuration and source files

--- pyomo_models/test_task_allocation.py
+++
@@ -1,8 +0,0 @@
-from unittest import TestCase
-
-from pyomo_models.task_allocation import Scenario
-
-
-class TestScenario(TestCase):
-    def test_from_file(self):
-        Scenario.from_file("../scenarios/task_allocation/test.yaml")
